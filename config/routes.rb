0ac6d2577a6fa6a0788f049e8d78f3efb87c8f19
Joshua Sadiua <joshuasadiua@gmail.com>
2017-10-29 23:46:43 +0000

    Part 2 Routing Lizards Completed

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'pages/home', as: 'home'
   root 'pages#home'
   get 'about_us', to: 'pages#about', as: 'about'
+  get 'lizards/:number', to: 'pages#lizards', as: 'lizards'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
