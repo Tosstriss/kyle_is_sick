5ebdf412c1decdca34ffcd132f7028fbae671afb
Joshua Sadiua <joshuasadiua@gmail.com>
2017-10-30 01:40:21 +0000

    Part 5 The Partially your Partial to my Partial figure out a lot of issues mostly an s on my pokemon_path

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about_us', to: 'pages#about', as: 'about'
   get 'lizards/:number', to: 'pages#lizards', as: 'lizards'
+  
+  get 'pokemons/', to: 'pokemons#index', as: 'pokemons'
+  get 'pokemons/:number', to: 'pokemons#show', as: 'pokemon'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
